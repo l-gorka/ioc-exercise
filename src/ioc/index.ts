51ffa2a5d623fa92a6f1f00a9032822f31135147
l-gorka <gorka.lukasz88@gmail.com>
2023-04-26 15:13:14 +0200

    solution

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,24 @@
 import IoCContainer from 'ioc-lite';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
+
+import { ApiConfig } from 'src/types';
+
+interface IoCResources {
+  logger: typeof Logger;
+  http: typeof HTTP;
+  users: typeof Users;
+  apiConfig: ApiConfig;
+}
 
 export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
-  // you can register some resources right now below...
+  const ioc = new IoCContainer<IoCResources>();
+
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('users', Users);
 
   return ioc;
 };
